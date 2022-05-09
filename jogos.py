3c1e5149bfc916feefc280bcd52d7abddc87e526
junior <99821795+hjunior29@users.noreply.github.com>
2022-05-09 16:41:56 -0300

    v1 do projeto -importado arquivos dos jogos -adicionado escolha dos jogos -chamando a função jogar dos arquivos

--- jogos.py
+++ jogos.py
@@ -1,6 +1,19 @@
+#importando arquivos
+import adivinhacao
+import forca
+
 print("********************************")
 print("********ESCOLHA UM JOGO*********")
 print("********************************")
 
 print("\nAdivinhção (1)\nForca (2)")
 jogo = int(input("Escolha um: "))
+
+if jogo == 1:
+    print("\nJogando Adivinhação\n\n")
+    #chamando o arquivo e chamndo a função do jogo
+    adivinhacao.jogar()
+if jogo == 2:
+    print("\nJogando Forca\n\n")
+    #chamando o arquivo e chamndo a função do jogo
+    forca.jogar()
