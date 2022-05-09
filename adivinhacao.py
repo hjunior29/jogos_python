50d5bfec5769d526e972545ff9c2c674bafdf0c7
junior <99821795+hjunior29@users.noreply.github.com>
2022-05-09 11:54:40 -0300

    v1.2 do jogo -adicionado laço while

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,23 +4,28 @@ print("********************************")
 
 #definindo o número secreto
 numero_secreto = 9
+tentativas = 5
+rodada = 1
 
-chute = int(input("Digite o seu número: "))
+while (rodada <= tentativas):
+    print("\nTentativa: {} de {}".format(rodada, tentativas))
+    chute = int(input("Digite o seu número: "))
 
-#print ("Você digitou", chute, "\nO número pensado foi", numero_secreto)
+    #testes
+    igual = chute == numero_secreto
+    maior = chute < numero_secreto
+    menor = chute > numero_secreto
 
-#testes
-igual = chute == numero_secreto
-maior = chute < numero_secreto
-menor = chute > numero_secreto
+    #condicoes
+    if (igual):
+        print("Você acertou!")
+    else:
+        if (menor):
+            print("Você errou!! O número secreto é MENOR que o número que você chutou")
+        elif (maior):
+            print("Você errou!! O número secreto é MAIOR que o número que você chutou")
 
-#condicoes
-if (igual):
-    print("Você acertou!")
-else:
-    if (menor):
-        print("Você errou!! O número secreto é MENOR que o número que você chutou")
-    elif (maior):
-        print("Você errou!! O número secreto é MAIOR que o número que você chutou")
+    #decremento
+    rodada = rodada + 1
 
 print("Fim de jogo!")
