0475318527926687a941d851bda1ce6502827b63
junior <99821795+hjunior29@users.noreply.github.com>
2022-05-09 11:05:44 -0300

    v1.1 do jogo -adicionado testes

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,13 +2,25 @@ print("********************************")
 print("BEM VINDO AO JOGO DE ADIVINHAÇÃO")
 print("********************************")
 
+#definindo o número secreto
 numero_secreto = 9
 
 chute = int(input("Digite o seu número: "))
 
-print ("Você digitou", chute, "\nO número pensado foi", numero_secreto)
+#print ("Você digitou", chute, "\nO número pensado foi", numero_secreto)
 
-if (numero_secreto == chute):
+#testes
+igual = chute == numero_secreto
+maior = chute < numero_secreto
+menor = chute > numero_secreto
+
+#condicoes
+if (igual):
     print("Você acertou!")
 else:
-    print("Você errou")
+    if (menor):
+        print("Você errou!! O número secreto é MENOR que o número que você chutou")
+    elif (maior):
+        print("Você errou!! O número secreto é MAIOR que o número que você chutou")
+
+print("Fim de jogo!")
