f826a81965dcd53b916dd678551671b672c8e8ae
junior <99821795+hjunior29@users.noreply.github.com>
2022-05-09 14:27:10 -0300

    v1.3 do jogo -testando laço for -adicionado parada quando acerta -adicionado condições para o chute

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,11 +5,13 @@ print("********************************")
 #definindo o número secreto
 numero_secreto = 9
 tentativas = 5
-rodada = 1
 
-while (rodada <= tentativas):
+for rodada in range(1, tentativas+1):
     print("\nTentativa: {} de {}".format(rodada, tentativas))
     chute = int(input("Digite o seu número: "))
+    if chute < 1 or chute > 100:
+        print("Você deve escolher um valor entre 1 e 100")
+        continue
 
     #testes
     igual = chute == numero_secreto
@@ -17,12 +19,13 @@ while (rodada <= tentativas):
     menor = chute > numero_secreto
 
     #condicoes
-    if (igual):
+    if igual:
         print("Você acertou!")
+        break
     else:
-        if (menor):
+        if menor:
             print("Você errou!! O número secreto é MENOR que o número que você chutou")
-        elif (maior):
+        elif maior:
             print("Você errou!! O número secreto é MAIOR que o número que você chutou")
 
     #decremento
