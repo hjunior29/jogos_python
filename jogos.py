a919be8b63aa50971dd9f275a9ab3fdc9c847958
junior <99821795+hjunior29@users.noreply.github.com>
2022-05-09 16:38:04 -0300

    Initial commit

---
+++ jogos.py
@@ -0,0 +1,6 @@
+print("********************************")
+print("********ESCOLHA UM JOGO*********")
+print("********************************")
+
+print("\nAdivinhção (1)\nForca (2)")
+jogo = int(input("Escolha um: "))
